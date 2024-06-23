a58cc9668b353b01c886010c7c59fd3c25cdd892
Jordan Miller <Jordan.kay@gmail.com>
2024-06-23 08:31:20 -0600

    built on iOS

--- mac/.satori/Satori.app/Contents/MacOS/launch.sh
+++ mac/.satori/Satori.app/Contents/MacOS/launch.sh
@@ -5,7 +5,29 @@
 PYTHON_SCRIPT_PATH="$(dirname "$0")/Satori"
 
 # Command to open the browser
-open "http://127.0.0.1:24601"
+#open "http://127.0.0.1:24601"
+
+# HTML content
+HTML_CONTENT='<!DOCTYPE html><html><head><title>Starting Satori Neuron</title><script type="text/javascript">setTimeout(function(){window.location.href = "http://127.0.0.1:24601";}, 1000 * 60);</script></head><body><p>Please wait a few minutes while the Satori Neuron boots up. <a href="http://127.0.0.1:24601">Refresh</a></p></body></html>'
+
+## Create a temporary file
+#TEMP_HTML_FILE=$(mktemp /tmp/temp_html.Satori.html)
+## Write the HTML content to the temporary file
+#echo "$HTML_CONTENT" > "$TEMP_HTML_FILE"
+## Command to open the temporary HTML file in the browser
+#open "$TEMP_HTML_FILE"
+
+# URL encode the HTML content
+URL_ENCODED_HTML=$(python3 -c "import urllib.parse; print(urllib.parse.quote('''$HTML_CONTENT'''))")
+
+# Create a data URL
+DATA_URL="data:text/html,$URL_ENCODED_HTML"
+
+# Command to open the data URL in the browser
+open "$DATA_URL"
 
 # Command to open Terminal and run the Python script
 osascript -e "tell application \"Terminal\" to do script \"$PYTHON_SCRIPT_PATH\""
+
+
+
